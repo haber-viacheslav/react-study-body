02fc5b129a72291307ec0024386f82064df22270
Haber Viacheslav <gaberslavik@gmail.com>
2023-01-10 18:11:06 +0200

    added new changes

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,20 +1,29 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import { Component } from 'react';
 export class Counter extends Component {
-  handleIncrement = e => {
+  state = {
+    value: 0,
+  };
+  handleIncrement = () => {
     console.log('Click on btn +1');
-    console.log(this);
-    console.log(e.target);
+    // console.log(this);
+    // console.log(e.target);
+    // const { target } = e;
+    // setTimeout(() => {
+    //   console.log(target);
+    // }, 2000);
+    this.setState({ value: this.state.value + 1 });
   };
   handleDecrement = e => {
     console.log('Click on btn -1');
-    console.log(this);
-    console.log(e.target);
+    // console.log(this);
+    // console.log(e.target);
+    this.setState({ value: this.state.value - 1 });
   };
   render() {
     return (
       <div className="Counter">
-        <span className="Counter__value">0</span>
+        <span className="Counter__value">{this.state.value}</span>
 
         <div className="Counter__controls">
           <button type="button" onClick={this.handleIncrement}>
